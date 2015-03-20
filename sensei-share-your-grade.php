5e061540789e60a24eb6eb6095b13d4a779fbd0a
David Page <david.page@psycle.com>
2015-03-20 13:07:13 +0000

    Fixes #8 with lesson 'has_passed' not getting set correctly

--- sensei-share-your-grade.php
+++ sensei-share-your-grade.php
@@ -336,25 +336,8 @@ final class Sensei_Share_Your_Grade {
 
 		// Find out if the user has passed the current lesson
 		$user_lesson_status = WooThemes_Sensei_Utils::user_lesson_status( $lesson_id, $user_id );
-		$user_lesson_end = WooThemes_Sensei_Utils::user_completed_lesson( $user_lesson_status );
-		if ( $user_lesson_end ) {
-			$lesson_quizzes = $woothemes_sensei->post_types->lesson->lesson_quizzes( $lesson_id );
-	        // Get Quiz ID
-	        if ( is_array( $lesson_quizzes ) || is_object( $lesson_quizzes ) ) {
-	            foreach ($lesson_quizzes as $quiz_item) {
-	                $lesson_quiz_id = $quiz_item->ID;
-	            } // End For Loop
-
-				// Get the user's grade
-		        $user_grade = WooThemes_Sensei_Utils::sensei_get_activity_value( array( 'post_id' => $lesson_quiz_id, 'user_id' => $user_id, 'type' => 'sensei_quiz_grade', 'field' => 'comment_content' ) );
-		        // Check if Grade is greater than pass percentage
-		        $pass_mark = abs( round( doubleval( get_post_meta( $lesson_quiz_id, '_quiz_passmark', true ) ), 2 ) );
-		        if ( $pass_mark <= intval( $user_grade ) ) {
-		            $has_passed = true;
-		        } // End If Statement
-		    }
-		}
-	    // No action required if the user hasn't passed the course
+		$has_passed = WooThemes_Sensei_Utils::user_completed_lesson( $user_lesson_status );
+		// No action required if the user hasn't passed the lesson
 		if ( false == $has_passed ) {
 			return;
 		}
